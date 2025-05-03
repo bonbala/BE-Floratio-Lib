22b578b16a5749752fe6c6e835f532d600637867
bonbala <nhathieu1805@gmail.com>
2025-05-03 15:32:53 +0700

    modified DocumentBuilder

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('Swagger Documentation')
+    .setDescription('The API description')
     .setVersion('1.0')
-    .addTag('cats')
+    .addTag('API')
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
